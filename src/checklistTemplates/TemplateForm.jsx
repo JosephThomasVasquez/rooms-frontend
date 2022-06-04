6c78ce03b0969d99d61f799492e173ea5ea4cbd3
Joseph Vasquez <joetv.tech@gmail.com>
2022-06-03 21:58:57 -0700

    Added form for tempalte name and description with sharable button

--- src/checklistTemplates/TemplateForm.jsx
+++ src/checklistTemplates/TemplateForm.jsx
@@ -9,7 +9,7 @@ const TemplateForm = () => {
     console.log("createdItems", createdItems);
   }, [itemsList, createdItems]);
 
-  const remap = (items) => {
+  const createChecklistItem = (items) => {
     let itemsArray = [];
     if (items.length >= 1) {
       items.map((item) => {
@@ -35,47 +35,80 @@ const TemplateForm = () => {
 
   const handleChange = ({ target }) => {
     const items = target.value.split(",");
-    // setItemsList(items);
     console.log("items", items);
 
-    remap(items);
+    createChecklistItem(items);
   };
 
-  const saveTemplate = () => {
-    // createChecklistItems(itemsList);
-    // console.log(itemsList);
-  };
+  const saveTemplate = () => {};
 
   return (
     <div className="container">
       <div className="row mb-3">
         <h2>New Template</h2>
       </div>
-      <div className="row d-flex justify-content-center align-items-center mb-5">
-        <div className="form-floating">
-          <textarea
-            className="form-control"
-            placeholder="Insert items separated by comma's here"
-            id="floatingTextarea"
-            onChange={handleChange}
-          ></textarea>
-          <label htmlFor="floatingTextarea" className="ms-2">
-            Items...
-          </label>
-        </div>
+      <div className="row mb-5">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="template_name" className="form-label">
+              Template Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="template_name"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="template_description" className="form-label">
+              Template Description
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="template_description"
+            />
+          </div>
+
+          <div className="col">
+            <label htmlFor="floatingTextarea" className="ms-2">
+              Insert Items
+            </label>
+            <textarea
+              className="form-control"
+              placeholder="Insert items separated by comma's here i.e. item1, item2, item3..."
+              id="floatingTextarea"
+              onChange={handleChange}
+            ></textarea>
+          </div>
+          <div className="row d-flex align-items-center mt-3">
+            <div className="col-1">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={saveTemplate}
+              >
+                Save
+              </button>
+            </div>
+
+            <div className="col-1 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="sharable-check"
+              />
+              <label className="form-check-label" htmlFor="sharable-check">
+                Sharable
+              </label>
+            </div>
+          </div>
+        </form>
       </div>
 
       <div className="row fs-4 fw-bold">
         <div className="col-1">Items</div>
-        <div className="col-8">
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={saveTemplate}
-          >
-            Save
-          </button>
-        </div>
       </div>
       <div className="row">{JSON.stringify(createdItems)}</div>
     </div>
