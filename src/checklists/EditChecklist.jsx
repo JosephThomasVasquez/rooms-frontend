caec89bc63e4323533313a515d802ab0c1e558f7
Joseph Vasquez <joetv.tech@gmail.com>
2022-06-05 10:45:25 -0700

    Added logic to toggle checked items and styling. There is a bug in mapping items as it creates true and false keys on item clicked

--- src/checklists/EditChecklist.jsx
+++ src/checklists/EditChecklist.jsx
@@ -7,8 +7,12 @@ import { CheckIcon, XIcon } from "@heroicons/react/solid";
 const EditChecklist = () => {
   const { checklistId } = useParams();
   const params = useParams();
-  console.log("params", params);
+
+  const initialCheckedItems = null;
+
   const [checklistDetails, setChecklistDetails] = useState(null);
+  const [percentChecked, setPercentChecked] = useState(0);
+  const [checkedItems, setCheckedItems] = useState({ ...initialCheckedItems });
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -33,6 +37,63 @@ const EditChecklist = () => {
     return () => abortController.abort();
   }, [checklistId]);
 
+  useEffect(() => {
+    if (checklistDetails?.items?.length > 0) {
+      const percentage = (2 / checklistDetails.items.length) * 100;
+      console.log(percentage);
+      makeCheckedItems();
+      setPercentChecked(percentage.toFixed());
+    }
+  }, [checklistDetails]);
+
+  const makeCheckedItems = async () => {
+    const items = await checklistDetails.items;
+
+    const mappedItems = {};
+
+    if (items) {
+      console.log("items:", items);
+      items.map((i) => {
+        return (mappedItems[i] = false);
+      });
+
+      setCheckedItems({ ...mappedItems });
+    }
+  };
+
+  const handleClickedItem = (item) => {
+    console.log("Search for:", item);
+
+    const isExistingItem = checklistDetails.items.find((i) => i === item);
+
+    console.log("item Exists >>>", isExistingItem);
+
+    if (item === isExistingItem) {
+      const toggleClicked = (checkedItems[item] = !checkedItems[item]);
+
+      setCheckedItems({
+        ...checkedItems,
+        [checkedItems[item]]: !checkedItems[item],
+      });
+    }
+
+    console.log(checkedItems);
+  };
+
+  const mapItems = checklistDetails?.items?.map((item) => (
+    <li
+      key={`checklist-item-id-${item}`}
+      name={item}
+      className={`row border rounded shadow-sm my-1 p-2 list-unstyled ${
+        checkedItems[item] ? "template-item-check" : "template-item"
+      }`}
+      onClick={() => handleClickedItem(item)}
+      value={item}
+    >
+      {item}
+    </li>
+  ));
+
   return (
     <div className="container">
       <h2>
@@ -50,23 +111,11 @@ const EditChecklist = () => {
             dayjs(checklistDetails?.date_completed).format("MMM DD, YYYY")}
         </div>
       </div>
+      <div className="row fw-bold">{percentChecked}% Completed</div>
       <div className="row mt-5 rounded-top shadow">
-        <p className="d-flex align-items-center rounded-top shadow">
-          <CheckIcon className="check-icon icon-bg-round-passed" />
-          <span className="fs-4 ms-2">
-            {checklistDetails?.passed
-              ? checklistDetails?.passed.length
-              : "none"}
-          </span>
-        </p>
-        <ul className="row">
-          {checklistDetails?.passed &&
-            checklistDetails?.passed.map((item) => (
-              <div key={`item-id-${item}`} className="col-3">
-                <li className="list-unstyled">{item}</li>
-              </div>
-            ))}
-        </ul>
+        <div className="row">{JSON.stringify(checkedItems)}</div>
+        {/* <CheckIcon className="check-icon icon-bg-round-passed" /> */}
+        <ul className="fs-4">{checklistDetails && mapItems}</ul>
       </div>
       <div className="row mt-5 rounded-top shadow">
         <p className="d-flex align-items-center rounded-top shadow">
