90a250b4f4ca7333e257cdda0078dd53595293eb
Joseph Vasquez <joetv.tech@gmail.com>
2022-06-12 15:23:16 -0700

    Added mroe fields to buildings cards from response

--- src/directory/buildings/BuildingCard.jsx
+++ src/directory/buildings/BuildingCard.jsx
@@ -9,7 +9,21 @@ const BuildingCard = ({ building }) => {
         alt={`Image of ${building.building_name}`}
       ></img>
       <div className="card-body">
-        <h2>{building.building_name}</h2>
+        <div className="row">
+          <div className="col">
+            <h2 className="">{building.building_name}</h2>
+            <div className="fs-5">{building.building_description}</div>
+          </div>
+          <div className="col">
+            <div className="row">
+              {building.address_number} {building.street_name}
+            </div>
+            <div className="row">
+              {building.city} {building.state}, {building.zipcode}
+            </div>
+            <div className="row">{building.country}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
