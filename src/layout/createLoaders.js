93e105a7ece2a189e2c5c0abbc9a07b31ec2832f
Joseph Vasquez <joetv.tech@gmail.com>
2022-07-12 17:05:17 -0700

    Updated styling of loaders

--- src/layout/createLoaders.js
+++ src/layout/createLoaders.js
@@ -3,22 +3,22 @@ import ContentLoader from "./ContentLoader";
 const createLoaders = (count) => {
   return (
     <>
-      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12">
+      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12 my-2">
         <ContentLoader />
       </div>
-      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12">
+      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12 my-2">
         <ContentLoader />
       </div>
-      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12">
+      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12 my-2">
         <ContentLoader />
       </div>
-      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12">
+      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12 my-2">
         <ContentLoader />
       </div>
-      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12">
+      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12 my-2">
         <ContentLoader />
       </div>
-      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12">
+      <div className="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12 my-2">
         <ContentLoader />
       </div>
     </>
